54baf594c70f46a99a741471f8493787e651f92b
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-11-20 09:23:01 +0500

    Add files via upload

---
+++ Lecture-no-2/Operators.cpp
@@ -0,0 +1,21 @@
+// Arithmetic :  + , -, *
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5 , b = 10;
+
+    // sum = a + b;
+    cout << "Sum = "<< (a+b) << endl;
+    cout << "Sub = "<< (a-b) << endl;
+    cout << "Mul = "<< (a*b) << endl;
+    cout << "div = "<< (a/b) << endl;
+    cout << "Reminder = "<< (a%b) << endl;
+
+    cout << (3 < 5) << endl;
+    cout << (3 > 5) << endl;
+    cout << (3 <= 3) << endl;
+
+    return 0;
+}

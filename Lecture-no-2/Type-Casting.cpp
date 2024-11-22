91a0bad5b01bb7342fae15c61e9dedad1dd2a182
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-11-22 10:58:29 +0500

    Add files via upload

---
+++ Lecture-no-2/Type-Casting.cpp
@@ -0,0 +1,20 @@
+// Type Casting : Converting data from one type to another.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // char grade = 'A';
+    // int value = grade;
+    // cout<< value << endl;
+
+    // double prince = 100.99;
+
+    // int newPrice = price;
+    double price;
+    cout<<"Enter the Price : ";
+    cin>> price;
+
+    cout<<"You Entered Price = " << price << endl;
+    return 0;
+}

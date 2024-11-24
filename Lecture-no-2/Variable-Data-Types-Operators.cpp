0e186d97dfbedbecc09604cc8294d57ffabcc689
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2024-11-24 10:22:09 +0500

    Add files via upload

---
+++ Lecture-no-2/Variable-Data-Types-Operators.cpp
@@ -0,0 +1,36 @@
+// Variable : Containers to store data is called Variable. y = 5, x = 56
+
+
+// Data Types : int, char, float, bool, double . This called Primitive Data Type.
+
+// Single Digit is called Bit . EXP : 1 Bit,
+
+// 8 Bit = 1 Byte . 
+
+// One integer place 4 Bytes.  4 Bytes is equal 32 bit.
+
+// Char : is to take Single value . Char is equal 1 Byte. EXP :  char Grad = 'A'
+
+// Float : is to take 4 Bytes values.
+
+// Bool : is to take value 1 Byte .
+
+// Double : is to take value 8 Byte.
+
+#include <iostream>
+using namespace std;
+
+int  main() {
+
+int a = 5; // integer variable
+char grade = 'A'; // character variable
+float PI = 3.14f; // FLoat variable
+bool isTrue = true; // boolean variable
+double d = 100.1234; // double variable
+cout << "Value of a = " << a << endl;
+cout << "Value of grade = " << grade << endl;
+cout << "Value of PI = " << PI << endl;
+cout << "Value of isTrue = " << isTrue << endl;
+cout << "Value of d = " << d << endl;
+    return 0;
+}

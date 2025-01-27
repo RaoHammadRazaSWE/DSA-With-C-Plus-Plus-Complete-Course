8950d4f609a0603adc1a70b36e9257c1cd5e60f2
Rao Hammad Raza <145425438+RaoHammadRazaSWE@users.noreply.github.com>
2025-01-27 07:43:18 +0500

    Add files via upload

---
+++ Lecture-no-7/Bitwise-operators/first.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a = 4, b = 8;
+
+    // cout << (a & b); // Ans : 0
+
+    // cout << (a | b);  // Ans : 12
+
+    // cout << (a ^ b);  // Ans : 12
+
+    // Bitwise <<  left shift operators.
+
+    // cout << (10 << 2) << endl;
+
+    // Bitwise >>  Right shift operators.
+
+    // cout << (10 >> 1) << endl;
+
+    // cout << (8 >> 2) << endl;
+
+    // Home Work
+
+    int a = 6, b = 10;
+
+    // // 6 & 10 :
+    // cout << (a & b); // Ans : 2
+
+    // 6 | 10 :
+    // cout << (a | b); // Ans : 14
+
+    // 6 ^ 10 :
+    // cout << (a ^ b); // Ans : 12
+
+    // Solve for :
+
+    // 10 << 2 :
+    // cout << (10 << 2) << endl; // 40
+
+    // 10 >> 1
+    cout << (10 >> 1) << endl; // 5
+
+    return 0;
+}
